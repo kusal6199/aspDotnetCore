3f9c76639e3c788dbf0c169ee4a9e1c248826419
kusal6199 <kushalkdl63@gmail.com>
2026-01-07 07:53:08 +0545

    Initial commit with project files

---
+++ CrudOperation/Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using CrudOperation.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CrudOperation.Data
+{
+	public class ApplicationDbContext : DbContext
+	{
+		public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+		{
+
+		}
+
+		public DbSet<Student>  Students{get; set;}
+	}
+}
+
---
+++ CrudOperation/Models/Student.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace CrudOperation.Models
+{
+	public class Student
+	{
+		public Student()
+		{
+		}
+
+		[Key]
+		
+		public int StudentId { get; set; }
+
+		[Required]
+		[Display(Name="Full Name")]
+		public String Name { get; set; }
+
+		[Display(Name="Email Address")]
+		[Required]
+		[EmailAddress]
+		public String Email { get; set; }
+
+		[Required]
+		[Display(Name ="Course Name")]
+		public String Course { get; set; }
+
+		[Required]
+		public DateTime EndrollementDate { get; set; }
+
+
+	}
+}
+
